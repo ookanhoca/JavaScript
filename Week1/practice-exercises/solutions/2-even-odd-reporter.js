fde00b87d8c98e81b0a99bb3eb1c6af4b2a4dca3
ookanhoca <128608466+ookanhoca@users.noreply.github.com>
2023-07-09 17:01:41 +0200

    Update 2-even-odd-reporter.js

--- Week1/practice-exercises/solutions/2-even-odd-reporter.js
+++ Week1/practice-exercises/solutions/2-even-odd-reporter.js
@@ -3,8 +3,8 @@
  *
  *  Create a for loop, that iterates from 0 to 20.
  *  Create a conditional statement that checks if the value of the counter variable is odd or even.
- *  If it's odd, log to the console The number [PUT_NUMBER_HERE] is odd!.
- *  If it's even, log to the console The number [PUT_NUMBER_HERE] is even!.
+ *  If it's odd, log to the console The number [22] is odd!.
+ *  If it's even, log to the console The number [5] is even!.
  */
 
 for (let i = 0; i < 21; i++) {
